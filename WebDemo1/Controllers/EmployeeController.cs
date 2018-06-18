0b258b63aca3161a7ad1b7dc20f9684ba252d5d5
Joseph <lai225joseph@gmail.com>
2018-06-18 11:54:28 +0800

    first commit

---
+++ WebDemo1/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebDemo1.Models;
+
+namespace WebDemo1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var E = new Employee() { Eid = 1, EmpName = "Maz Trainer" };
+            return View(E);
+        }
+    }
+}
---
+++ WebDemo1/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDemo1.Models
+{
+    public class Employee
+    {
+        public int Eid { get; set; }
+        public string EmpName { get; set; }
+    }
+}
